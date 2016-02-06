1582da80e858117c475c058ccd1d9cabfb003a78
lplamondon <lplamond@sfu.ca>
2016-02-06 04:54:40 +0000

    Possible fix for validation

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,19 +3,19 @@ class Person < ActiveRecord::Base
                     length: {minimum: 1}
                     
     validates :weight, presence: true,
-                    length: {minimum: 1, maximum: 5}, numericality: true, greater_than_or_equal_to: 0
+                    length: {minimum: 1}, numericality: true, greater_than_or_equal_to: 0
                     
     validates :height, presence: true,
-                    length: {minimum: 1, maximum: 5}, numericality: true, greater_than_or_equal_to: 0
+                    length: {minimum: 1}, numericality: true, greater_than_or_equal_to: 0
                     
     validates :red, presence: true,
-                    length: {minimum: 1, maximum: 5}, numericality: {only_integer: true}, greater_than_or_equal_to: 0, less_than: 256
+                    length: {minimum: 1}, numericality: {only_integer: true}, greater_than_or_equal_to: 0, less_than: 256
                     
     validates :green, presence: true,
-                    length: {minimum: 1, maximum: 5}, numericality: {only_integer: true}, greater_than_or_equal_to: 0, less_than: 256
+                    length: {minimum: 1}, numericality: {only_integer: true}, greater_than_or_equal_to: 0, less_than: 256
                     
     validates :blue, presence: true,
-                    length: {minimum: 1, maximum: 5}, numericality: {only_integer: true}, greater_than_or_equal_to: 0, less_than: 256
+                    length: {minimum: 1}, numericality: {only_integer: true}, greater_than_or_equal_to: 0, less_than: 256
                     
     validates :country, presence: true,
                     length: {minimum: 1}
