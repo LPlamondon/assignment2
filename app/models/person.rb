1e2e60b90d48ca4b7b2beefa79b7a15f48f7e315
lplamondon <lplamond@sfu.ca>
2016-02-06 05:24:43 +0000

    Posible fix for validation from Thomas

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,20 +2,20 @@ class Person < ActiveRecord::Base
     validates :name, presence: true,
                     length: {minimum: 1}
                     
-    #validates :weight, presence: true,
-    #                length: {minimum: 1}, numericality: true, greater_than_or_equal_to: 0
+    validates :weight, presence: true,
+                    length: {minimum: 1}, numericality: {greater_than_or_equal_to: 0}
                     
    # validates :height, presence: true,
-   #                 length: {minimum: 1}, numericality: true, greater_than_or_equal_to: 0
+   #                 length: {minimum: 1}, numericality: {greater_than_or_equal_to: 0}
                     
    # validates :red, presence: true,
    #                 length: {minimum: 1}, numericality: {only_integer: true}, greater_than_or_equal_to: 0, less_than: 256
                     
    # validates :green, presence: true,
-   #                 length: {minimum: 1}, numericality: {only_integer: true}, greater_than_or_equal_to: 0, less_than: 256
+   #                 length: {minimum: 1}, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than: 256}
                     
    # validates :blue, presence: true,
-   #                 length: {minimum: 1}, numericality: {only_integer: true}, greater_than_or_equal_to: 0, less_than: 256
+   #                 length: {minimum: 1}, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than: 256}
                     
    # validates :country, presence: true,
    #                 length: {minimum: 1}
@@ -24,7 +24,7 @@ class Person < ActiveRecord::Base
    #                 length: {minimum: 1}
                     
    # validates :age, presence: true,
-   #                 length: {minimum: 1}, numericality: {only_integer: true}, greater_than_or_equal_to: 0
+   #                 length: {minimum: 1}, numericality: {only_integer: true, greater_than_or_equal_to: 0}
                     
    # validates :language, presence: true,
    #                 length: {minimum: 1}
