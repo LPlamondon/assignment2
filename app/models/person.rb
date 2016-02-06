26a7b2f43185a7a43845251acae83a8c6a7a4a86
lplamondon <lplamond@sfu.ca>
2016-02-06 04:40:39 +0000

    First attempt at some validation

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,32 @@
 class Person < ActiveRecord::Base
     validates :name, presence: true,
                     length: { minimum: 1 }
+                    
+    validates :weight, presence: true,
+                    length: {minimum: 1, maximum: 5, numericality: true}
+                    
+    validates :height, presence: true,
+                    length: {minimum: 1, maximum: 5, numericality: true}
+                    
+    validates :red, presence: true,
+                    length: {minimum: 1, maximum: 5, numericality: {only_integer: true}}
+                    
+    validates :green, presence: true,
+                    length: {minimum: 1, maximum: 5, numericality: {only_integer: true}}
+                    
+    validates :blue, presence: true,
+                    length: {minimum: 1, maximum: 5, numericality: {only_integer: true}}
+                    
+    validates :country, presence: true,
+                    length: {minimum: 1, maximum: 5}
+                    
+    validates :gender, presence: true,
+                    length: {minimum: 1}
+                    
+    validates :age, presence: true,
+                    length: {minimum: 1, maximum: 5, numericality: {only_integer: true}}
+                    
+    validates :language, presence: true,
+                    length: {minimum: 1}
+                    
 end
